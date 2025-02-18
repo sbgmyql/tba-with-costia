5244390ce51995e41b503f30f28d64ab6129c7a3
sbgmyql <sbgmyql@gmail.com>
2025-02-18 20:00:29 +0200

    an upgrade to the shell to remove if there is old projects and a change to README.md

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # tba-with-costia
 
-to download it on windows for vishul studio run 	 
+to download it on windows for vishul studio run 	 (assuming git **is not** installed):
 ```PS
 winget install git.git && git clone https://github.com/sbgmyql/tba-with-costia && cd tba-with-costia && powershell -ExecutionPolicy Bypass -File install.ps1 
+```
+beware the commend instells git so it only needed to be run once 
+
+assuming git **is** installed :
+```PS
+git clone https://github.com/sbgmyql/tba-with-costia && cd tba-with-costia && powershell -ExecutionPolicy Bypass -File install.ps1 
 ```
--- install.ps1
+++ install.ps1
@@ -1,10 +1,16 @@
-$RED = "Red"
+$RED = [ConsoleColor]::Red
+$GREEN = [ConsoleColor]::Green
 
 if (Test-Path "$env:USERPROFILE\source\repos") {
+    if (Test-Path "$env:USERPROFILE\source\repos\tba-with-costia") {
+        Remove-Item -Recurse -Force "$env:USERPROFILE\source\repos\tba-with-costia"
+    }
     Set-Location ..
-    Move-Item -Path "tba-with-costia" -Destination "$env:USERPROFIL\source\repos"
-    Set-Location "C:$env:USERPROFILE\source\repos"  
-    Clear-Host 
+    Move-Item -Path "tba-with-costia" -Destination "$env:USERPROFILE\source\repos"
+    Set-Location "$env:USERPROFILE\source\repos"
+    Clear-Host
+    Write-Host "All passed, tell Michael he did a good job" -ForegroundColor $GREEN
 } else {
     Write-Host "[Something is wrong]" -ForegroundColor $RED
 }
+
